ea6373e44ed7e7628f0c4831700b05b230078d02
24k40 <rastochca.ru>
2023-06-09 14:00:16 +0700

    add task5

---
+++ src/main/java/org/example/task5/Main.java
@@ -0,0 +1,28 @@
+package org.example.task5;
+
+import org.example.task5.webPage.WebPage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static final Map<String, List<WebPage>> mapContents = new HashMap<>();
+    public static void main(String[] args) {
+        addWebPageInList(new WebPage("qqq", "tns", "Рекурсивные функции тоже используют стек вызовов! Посмотрим, как это\n" +
+                "делается, на примере функции вычисления факториала. Вызов factorial(5)\n" +
+                "записывается в виде 5! и определяется следующим образом: 5! = 5*4*3*2*1.\n" +
+                "По тому же принципу factorial(3) соответствует 3*2*1. Рекурсивная функция для вычисления факториала числа выглядит так:"));
+
+        addWebPageInList(new WebPage("rrr", "pimp", "Здесь важно, что каждый вызов создает собственную копию x. Обратиться\n" +
+                "к переменной x, принадлежащей другой функции, невозможно.\n" +
+                "Стек играет важную роль в рекурсии. В начальном примере были представлены два решения поиска ключа. Вспомните, как выглядел первый:"));
+
+
+
+    }
+    private static void addWebPageInList(WebPage webPage) {
+        String[] arrContent = webPage.getContent().split(" ");
+
+    }
+}
---
+++ src/main/java/org/example/task5/webPage/WebPage.java
@@ -0,0 +1,46 @@
+package org.example.task5.webPage;
+
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+
+    public WebPage(String url, String title, String content) {
+        this.url = url;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "WebPage{" +
+                "url='" + url + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
