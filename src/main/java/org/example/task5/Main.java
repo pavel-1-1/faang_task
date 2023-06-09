bf3f809c396ce8986155954b130462ae0cc80d46
24k40 <rastochca.ru>
2023-06-09 14:53:00 +0700

    add task5

--- src/main/java/org/example/task5/Main.java
+++ src/main/java/org/example/task5/Main.java
@@ -2,13 +2,19 @@ package org.example.task5;
 
 import org.example.task5.webPage.WebPage;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     private static final Map<String, List<WebPage>> mapContents = new HashMap<>();
+
     public static void main(String[] args) {
+        mapContents.put("функции", new ArrayList<>());
+        mapContents.put("создает", new ArrayList<>());
+        mapContents.put("факториала", new ArrayList<>());
+
         addWebPageInList(new WebPage("qqq", "tns", "Рекурсивные функции тоже используют стек вызовов! Посмотрим, как это\n" +
                 "делается, на примере функции вычисления факториала. Вызов factorial(5)\n" +
                 "записывается в виде 5! и определяется следующим образом: 5! = 5*4*3*2*1.\n" +
@@ -19,10 +25,17 @@ public class Main {
                 "Стек играет важную роль в рекурсии. В начальном примере были представлены два решения поиска ключа. Вспомните, как выглядел первый:"));
 
 
-
     }
-    private static void addWebPageInList(WebPage webPage) {
-        String[] arrContent = webPage.getContent().split(" ");
 
+    private static void addWebPageInList(WebPage webPage) {
+        String[] arrContent = webPage.getContent().replace(",", "").split(" ");
+        for (int i = 0; i < arrContent.length; i++) {
+            if (mapContents.containsKey(arrContent[i])) {
+                mapContents.get(arrContent[i]).add(webPage);
+            }
+        }
+    }
+    private static void searchKey(String key) {
+        if (mapContents.containsKey())
     }
 }
