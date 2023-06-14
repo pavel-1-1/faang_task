6de49e891bb30813c172a2e91515125162255c04
24k40 <rastochca.ru>
2023-06-14 11:04:31 +0700

    add abstraction

---
+++ src/main/java/org/example/Abstraction/character/Archer.java
@@ -0,0 +1,14 @@
+package org.example.Abstraction.character;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public int attack(Character character) {
+        character.setHealth(character.getHealth() - this.getDexterity());
+        return character.getHealth();
+    }
+
+}
